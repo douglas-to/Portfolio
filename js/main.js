7d79244a32e17b17e8a82806cef5c09868e44e01
Douglas Trigueiro <dougoliveira564@gmail.com>
2023-01-27 19:38:14 -0300

    Versão 2

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ function scrollEffect(){
 	
 	window.sr = ScrollReveal({ reset: true });
 
-	sr.reveal(".section1", { delay: 350 });
-	sr.reveal(".section2", { delay: 350 });
-	sr.reveal(".section3", { delay: 350 });
-	sr.reveal(".footer", { delay: 350 })	
+	sr.reveal(".section1", { delay: 250 });
+	sr.reveal(".section2", { delay: 250 });
+	sr.reveal(".section3", { delay: 250 });
+	sr.reveal(".footer", { delay: 250 })	
 }
 
 scrollEffect();
