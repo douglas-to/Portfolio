ab2bc7e25786b5df9e8f15b5dd838e79c5c0c5f0
Douglas Trigueiro <dougoliveira564@gmail.com>
2023-09-07 20:37:18 -0300

    Atualização sessão de projetos

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-// Api Github
-const ul = document.querySelector("ul");
-
-fetch("https://api.github.com/users/douglas-to/repos")
-.then(async res => {
-	
-	if(!res.ok){
-		throw new Error(res.status);
-	}
-
-	let data = await res.json();
-
-	data.map(item => {
-		let li = document.createElement("li");
-
-		li.className = 'font-style-title fs-5';
-
-			li.innerHTML = `
-				<h5 class="mt-5">${item.name}</h5>
-				<p>Data de criação: ${
-					Intl.DateTimeFormat('pt-br')
-					.format(new Date(item.created_at))
-				}</p>
-				<a class="buttonClass rounded-4 fs-6 font-style-title" href="${item.html_url}"><i class="fa-brands fa-github me-2"></i> Ver projeto</a>
-			`
-
-			ul.appendChild(li);
-	})
-
-}).catch(e => console.log(e))
-
 
 // Scroll Effect
 
